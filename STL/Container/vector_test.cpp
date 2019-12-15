707dc4ebf5c2f22b8a2116b80473b21e363e774a
wang <1411288042.qq@com>
2019-12-15 12:04:42 +0800

    test

--- STL/Container/vector_test.cpp
+++ STL/Container/vector_test.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(){
+    vector<string> vecStr = {"hhh","aaa","bbbb","dddd"};
+    vector<string>::iterator iter = vecStr.begin();
+    
+    for(;iter!=vecStr.end();iter++){
+        cout<<*iter<<endl;
+    }
     cout<<"hello world"<<endl;
     system("pause");
     return 0;
